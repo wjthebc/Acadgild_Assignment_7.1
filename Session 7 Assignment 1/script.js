eb45f8c2efde1cc6855ec8c480604ec8d3d33dd1
wjthebc <40471359+wjthebc@users.noreply.github.com>
2018-08-11 13:57:29 -0400

    Add files via upload

---
+++ Session 7 Assignment 1/script.js
@@ -0,0 +1,22 @@
+var num1 = -20, num2 = 10, num3 = 100, num4 = -5, num5 = 200;
+
+if (num1 > num2 && num1 > num3 && num1 > num4 && num1 > num5)
+  {
+    console.log(num1);
+  }
+else if (num2 > num1 && num2 > num3 && num2 > num4 && num2 > num5)
+  {
+    console.log(num2);
+  }
+else if (num3 > num1 && num3 > num2 && num3 > num4 && num3 > num5)
+  {
+    console.log(num3);
+  }
+else if (num4 > num1 && num4 > num2 && num4 > num3 && num4 > num5)
+  {
+    console.log(num4);
+  }
+else if (num5 > num1 && num5 > num2 && num5 > num3 && num5 > num4)
+  {
+    console.log(num5);
+  }
